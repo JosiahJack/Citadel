a58809f9e9efc90c9f382cc9d9ee5878a710bc86
JosiahJack <josiahjack@siu.edu>
2016-03-07 18:57:54 -0600

    Image Effects Commit JACK 7.MAR.72
    Imported standard effects package.  Implemented image after effects such
    as Anti Aliasing, tad bit of Bloom (subtle), and subtle Ambient Screen
    Space Obscurance.  Will use this as a base to add effects later such as
    the berserk patch halucinations.  Added more lights to Reactor Level.

---
+++ Assets/Scripts/TeleportTouch.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportTouch : MonoBehaviour {
+	public Transform targetDestination; // assign in the editor
+	public bool playSound;
+	public AudioClip SoundFX;
+	private AudioSource SoundFXSource;
+
+	void Awake () {
+		SoundFXSource = GetComponent<AudioSource>();
+	}
+
+	void  OnTriggerEnter ( Collider col  ){
+		if ((col.gameObject.tag == "Player") & (col.gameObject.GetComponent<PlayerHealth>().health > 0f)) {
+			col.transform.position = targetDestination.position;
+			if (playSound)
+				SoundFXSource.PlayOneShot(SoundFX);
+		}
+	}
+}
---
+++ Assets/Scripts/TextureScrollingFX.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextureScrollingFX : MonoBehaviour {
+	public float interval = 0.1f;
+	public float scrollSpeedX = 0.5f;
+	public float scrollSpeedY = 0f;
+	public float rotate;
+	private RawImage image;
+	private Vector2 offset;
+	private float updateTime;
+	private Texture2D texture;
+	private Rect tempRect;
+	//private Rect actRect;
+
+	void Awake () {
+		//texture = GetComponent<MeshRenderer>().material.mainTexture;
+		image = GetComponent<RawImage>();
+		//actRect = image.uvRect;
+		//FillTexture();
+		updateTime = Time.time + interval;
+	}
+
+	void OnEnable () {
+		//texture = GetComponent<MeshRenderer>().material.mainTexture;
+		image = GetComponent<RawImage>();
+		//actRect = image.uvRect;
+		//FillTexture();
+		updateTime = Time.time + interval;
+	}
+
+	void FillTexture () {
+		//texture.filterMode = FilterMode.Point;
+		offset.x += (Time.deltaTime*scrollSpeedX)/10.0f;
+		offset.y += (Time.deltaTime*scrollSpeedY)/10.0f;
+		tempRect.x += offset.x;
+		tempRect.y += offset.y;
+		//actRect = tempRect;
+		//texture.
+		//texture.Apply();
+	}
+
+	void Update () {
+		if (updateTime < Time.time) {
+			updateTime = (Time.time + interval);
+			FillTexture();
+		}
+	}
+}
