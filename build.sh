90c19ba408897c076246cffbfdd6a89e06dc768d
Josiah Jack <josiahjackcitadel@gmail.com>
2022-11-12 11:26:22 -0600

    Update to use correct dockerhub repo

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ declare -a components=("linux-il2cpp" "mac-mono" "windows-mono")
 for component in "${components[@]}"
 do
   GAME_CI_UNITY_EDITOR_IMAGE=unityci/editor:ubuntu-${UNITY_VERSION}-${component}-${GAME_CI_VERSION}
-  IMAGE_TO_PUBLISH=${MY_USERNAME}/editor:ubuntu-${UNITY_VERSION}-${component}-${GAME_CI_VERSION}
+  IMAGE_TO_PUBLISH=${MY_USERNAME}/unity3d:ubuntu-${UNITY_VERSION}-${component}-${GAME_CI_VERSION}
   docker build --build-arg GAME_CI_UNITY_EDITOR_IMAGE=${GAME_CI_UNITY_EDITOR_IMAGE} . -t ${IMAGE_TO_PUBLISH}
 # uncomment the following to publish the built images to docker hub
   docker push ${IMAGE_TO_PUBLISH}
