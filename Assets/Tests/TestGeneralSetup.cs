601135c201d87747df5df562b71253b19bb8b2d9
JosiahJack <josiahjackcitadel@gmail.com>
2023-05-17 06:58:56 -0500

    Update to add more tests and clean things up

--- Assets/Tests/TestGeneralSetup.cs
+++ Assets/Tests/TestGeneralSetup.cs
@@ -17,11 +17,14 @@ using UnityEditor.SceneManagement;
 #endif
 
 namespace Tests {
-    public class TestGeneralSetup {
-        public CitadelTests tester;
+    [SetUpFixture]
+    public class TestSetup {
+        public List<GameObject> allGOs;
+        private List<GameObject> allParents;
+        private bool acquiredGOs;
 
-        [Test]
-        public void ConfirmTargetnamesTargetted() {
+        [OneTimeSetup]
+        public void RunBeforeAnyTests() {
             // Use the Assert class to test conditions.
             // Use yield to skip a frame.
             GameObject go = GameObject.Find("GlobalConsts");
@@ -41,9 +44,14 @@ namespace Tests {
                     allGOs.Add(compArray[k].gameObject); // Add to full list, separate so we don't infinite loop
                 }
             }
+        }
+    }
 
-            Assert.That(allGOs.Count > 1,"CitadelTests.SetupLists failed to populate allGOs:" + allGOs.Count.ToString());
-
+    public class TestGeneralSetup {
+        [Test]
+        public void ConfirmTargetnamesTargetted() {
+            int i;
+            Assert.That(TestSetup.allGOs.Count > 1,"TestSetup.RunBeforeAnyTests failed to populate allGOs:" + TestSetup.allGOs.Count.ToString());
             ButtonSwitch bsTemp = null;
             ChargeStation csTemp = null;
             CyberAccess caTemp = null;
@@ -63,8 +71,8 @@ namespace Tests {
             Trigger trgTemp = null;
             TriggerCounter trgcTemp = null;
             HashSet<string> targetNames = new HashSet<string>();
-            for (i=0;i<allGOs.Count;i++) {
-                tio = allGOs[i].GetComponent<TargetIO>();
+            for (i=0;i<TestSetup.allGOs.Count;i++) {
+                tio = TestSetup.allGOs[i].GetComponent<TargetIO>();
                 if (tio == null) continue;
 
                 if (!string.IsNullOrWhiteSpace(tio.targetname)) {
@@ -73,63 +81,63 @@ namespace Tests {
             }
 
             HashSet<string> targets = new HashSet<string>();
-            for (int m=0;m<allGOs.Count;m++) {
-                bsTemp = allGOs[m].GetComponent<ButtonSwitch>();
+            for (int m=0;m<TestSetup.allGOs.Count;m++) {
+                bsTemp = TestSetup.allGOs[m].GetComponent<ButtonSwitch>();
                 if (bsTemp != null) {
                     if (!string.IsNullOrWhiteSpace(bsTemp.target)) {
                         targets.Add(bsTemp.target);
                     }
                 }
 
-                csTemp = allGOs[m].GetComponent<ChargeStation>();
+                csTemp = TestSetup.allGOs[m].GetComponent<ChargeStation>();
                 if (csTemp != null) {
                     if (!string.IsNullOrWhiteSpace(csTemp.target)) {
                         targets.Add(csTemp.target);	
                     }
                 }
 
-                caTemp = allGOs[m].GetComponent<CyberAccess>();
+                caTemp = TestSetup.allGOs[m].GetComponent<CyberAccess>();
                 if (caTemp != null) {
                     if (!string.IsNullOrWhiteSpace(caTemp.target)) {
                         targets.Add(caTemp.target);	
                     }
                 }
 
-                cswTemp = allGOs[m].GetComponent<CyberSwitch>();
+                cswTemp = TestSetup.allGOs[m].GetComponent<CyberSwitch>();
                 if (cswTemp != null) {
                     if (!string.IsNullOrWhiteSpace(cswTemp.target)) {
                         targets.Add(cswTemp.target);	
                     }
                 }
 
-                drTemp = allGOs[m].GetComponent<Door>();
+                drTemp = TestSetup.allGOs[m].GetComponent<Door>();
                 if (drTemp != null) {
                     if (!string.IsNullOrWhiteSpace(drTemp.target)) {
                         targets.Add(drTemp.target);	
                     }
                 }
 
-                hmTemp = allGOs[m].GetComponent<HealthManager>();
+                hmTemp = TestSetup.allGOs[m].GetComponent<HealthManager>();
                 if (hmTemp != null) {
                     if (!string.IsNullOrWhiteSpace(hmTemp.targetOnDeath)) {
                         targets.Add(hmTemp.targetOnDeath);	
                     }
                 }
-                ipTemp = allGOs[m].GetComponent<InteractablePanel>();
+                ipTemp = TestSetup.allGOs[m].GetComponent<InteractablePanel>();
                 if (ipTemp != null) {
                     if (!string.IsNullOrWhiteSpace(ipTemp.target)) {
                         targets.Add(ipTemp.target);	
                     }
                 }
 
-                keTemp = allGOs[m].GetComponent<KeypadElevator>();
+                keTemp = TestSetup.allGOs[m].GetComponent<KeypadElevator>();
                 if (keTemp != null) {
                     if (!string.IsNullOrWhiteSpace(keTemp.lockedTarget)) {
                         targets.Add(keTemp.lockedTarget);	
                     }
                 }
 
-                kkTemp = allGOs[m].GetComponent<KeypadKeycode>();
+                kkTemp = TestSetup.allGOs[m].GetComponent<KeypadKeycode>();
                 if (kkTemp != null) {
                     if (!string.IsNullOrWhiteSpace(kkTemp.target)) {
                         targets.Add(kkTemp.target);	
@@ -140,7 +148,7 @@ namespace Tests {
                     }
                 }
 
-                lbTemp = allGOs[m].GetComponent<LogicBranch>();
+                lbTemp = TestSetup.allGOs[m].GetComponent<LogicBranch>();
                 if (lbTemp != null) {
                     if (!string.IsNullOrWhiteSpace(lbTemp.target)) {
                         targets.Add(lbTemp.target);	
@@ -151,35 +159,35 @@ namespace Tests {
                     }
                 }
 
-                lrTemp = allGOs[m].GetComponent<LogicRelay>();
+                lrTemp = TestSetup.allGOs[m].GetComponent<LogicRelay>();
                 if (lrTemp != null) {
                     if (!string.IsNullOrWhiteSpace(lrTemp.target)) {
                         targets.Add(lrTemp.target);	
                     }
                 }
 
-                ltTemp = allGOs[m].GetComponent<LogicTimer>();
+                ltTemp = TestSetup.allGOs[m].GetComponent<LogicTimer>();
                 if (ltTemp != null) {
                     if (!string.IsNullOrWhiteSpace(ltTemp.target)) {
                         targets.Add(ltTemp.target);	
                     }
                 }
 
-                pgpTemp = allGOs[m].GetComponent<PuzzleGridPuzzle>();
+                pgpTemp = TestSetup.allGOs[m].GetComponent<PuzzleGridPuzzle>();
                 if (pgpTemp != null) {
                     if (!string.IsNullOrWhiteSpace(pgpTemp.target)) {
                         targets.Add(pgpTemp.target);	
                     }
                 }
 
-                pwpTemp = allGOs[m].GetComponent<PuzzleWirePuzzle>();
+                pwpTemp = TestSetup.allGOs[m].GetComponent<PuzzleWirePuzzle>();
                 if (pwpTemp != null) {
                     if (!string.IsNullOrWhiteSpace(pwpTemp.target)) {
                         targets.Add(pwpTemp.target);	
                     }
                 }
 
-                qbrTemp = allGOs[m].GetComponent<QuestBitRelay>();
+                qbrTemp = TestSetup.allGOs[m].GetComponent<QuestBitRelay>();
                 if (qbrTemp != null) {
                     if (!string.IsNullOrWhiteSpace(qbrTemp.target)) {
                         targets.Add(qbrTemp.target);	
@@ -190,14 +198,14 @@ namespace Tests {
                     }
                 }
 
-                trgTemp = allGOs[m].GetComponent<Trigger>();
+                trgTemp = TestSetup.allGOs[m].GetComponent<Trigger>();
                 if (trgTemp != null) {
                     if (!string.IsNullOrWhiteSpace(trgTemp.target)) {
                         targets.Add(trgTemp.target);	
                     }
                 }
 
-                trgcTemp = allGOs[m].GetComponent<TriggerCounter>();
+                trgcTemp = TestSetup.allGOs[m].GetComponent<TriggerCounter>();
                 if (trgcTemp != null) {
                     if (!string.IsNullOrWhiteSpace(trgcTemp.target)) {
                         targets.Add(trgcTemp.target);	
@@ -234,45 +242,25 @@ namespace Tests {
 
         [Test]
         public void ConfirmAIControllersSetupProperly() {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            GameObject go = GameObject.Find("GlobalConsts");
-            if (go != null) tester = go.GetComponent<CitadelTests>();
-            Assert.That(tester != null,"Unable to find CitadelTests attached to a GameObject GlobalConsts in scene");
+            Assert.That(TestSetup.allGOs.Count > 1,"TestSetup.RunBeforeAnyTests failed to populate TestSetup.allGOs:" + TestSetup.allGOs.Count.ToString());
             int i=0;
-            int k=0;
-            List<GameObject> allGOs = new List<GameObject>();
-            //Scene citmain = SceneManager.GetActiveScene();
-            Scene citmain = SceneManager.GetSceneByName("CitadelScene");
-            Assert.That(citmain.name == "CitadelScene","Not testing main Citadel scene, instead testing: " + citmain.name);
-            List<GameObject> allParents = citmain.GetRootGameObjects().ToList();
-            Assert.That(allParents.Count > 1,"Failed to populate allParents:" + allParents.Count.ToString());
-            for (i=0;i<allParents.Count;i++) {
-                Component[] compArray = allParents[i].GetComponentsInChildren(typeof(Transform),true);
-                for (k=0;k<compArray.Length;k++) {
-                    allGOs.Add(compArray[k].gameObject); // Add to full list, separate so we don't infinite loop
-                }
-            }
-
-            Assert.That(allGOs.Count > 1,"CitadelTests.SetupLists failed to populate allGOs:" + allGOs.Count.ToString());
 		    string script = "AIController";
 
             // Run through all GameObjects and perform all tests
-            for (i=0;i<allGOs.Count;i++) {
-                AIController aic = allGOs[i].GetComponent<AIController>();
+            for (i=0;i<TestSetup.allGOs.Count;i++) {
+                AIController aic = TestSetup.allGOs[i].GetComponent<AIController>();
                 if (aic != null) {
-                    Assert.That(!BoundsError(script,allGOs[i],0,28,aic.index,"index"),aic.gameObject.name + " has issue with AIController.index out of bounds [0,28]");
-                    //if (MissingComponent(script,allGOs[i],typeof(Rigidbody)))				issueCount_AIController++;
-                    //if (MissingComponent(script,allGOs[i],typeof(HealthManager)))			issueCount_AIController++;
-                    //if (aic.walkPathOnStart) {
-                    //    if (aic.walkWaypoints.Length < 1) {
-                    //        UnityEngine.Debug.Log(script + " missing any walkWaypoints but walkPathOnStart is true."); issueCount_AIController++;
-                    //    } else {
-                    //        for (k=0;k<aic.walkWaypoints.Length;k++) {
-                    //            if (aic.walkWaypoints[k] == null) UnityEngine.Debug.Log(script + " missing walkWaypoints["+k.ToString()+"]."); issueCount_AIController++;
-                    //        }
-                    //    }
-                    //}
+                    Assert.That(!BoundsError(script,TestSetup.allGOs[i],0,28,aic.index,"index"),aic.gameObject.name + " has issue with AIController.index out of bounds [0,28]");
+                     Assert.That(!MissingComponent(script,TestSetup.allGOs[i],typeof(Rigidbody)));
+                     Assert.That(!MissingComponent(script,TestSetup.allGOs[i],typeof(HealthManager)));
+                    if (aic.walkPathOnStart) {
+                        Assert.That(!(aic.walkWaypoints.Length < 1),script + " missing any walkWaypoints but walkPathOnStart is true.");
+                        if (aic.walkWaypoints.Length > 0) {
+                            for (k=0;k<aic.walkWaypoints.Length;k++) {
+                                Assert.That(aic.walkWaypoints[k] != null,script + " missing walkWaypoints["+k.ToString()+"].");
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -283,30 +271,42 @@ namespace Tests {
         //public IEnumerator TestGeneralSetupWithEnumeratorPasses() {
             //// Use the Assert class to test conditions.
             //// Use yield to skip a frame.
-            //GameObject go = GameObject.Find("GlobalConsts");
-            //if (go != null) tester = go.GetComponent<CitadelTests>();
-            //Assert.That(tester != null,"Unable to find CitadelTests attached to a GameObject GlobalConsts in scene");
-            //int i=0;
-            //int k=0;
-            //List<GameObject> allGOs = new List<GameObject>();
-            //List<GameObject> allParents = SceneManager.GetActiveScene().GetRootGameObjects().ToList();
-            //Assert.That(allParents.Count > 1,"Failed to populate allParents:" + allParents.Count.ToString());
-            //for (i=0;i<allParents.Count;i++) {
-            //    Component[] compArray = allParents[i].GetComponentsInChildren(typeof(Transform),true);
-            //    for (k=0;k<compArray.Length;k++) {
-            //        allGOs.Add(compArray[k].gameObject); // Add to full list, separate so we don't infinite loop
-            //    }
-            //}
-
-            //Assert.That(allGOs.Count > 1,"CitadelTests.SetupLists failed to populate allGOs:" + allGOs.Count.ToString());
         //    yield return null;
         //}
+
+        private bool MissingReference(string className, GameObject go, Component comp, string variableName) {
+            string self = (go != null) ? go.name : "errname";
+            string parent = (go.transform.parent != null) ? go.transform.parent.name : "none";
+            if (comp == null) {
+                Assert.That(false,"BUG: " + className + " is missing reference " + comp.ToString() + "(" + variableName + ") on GameObject " + self + " with parent of " + parent);
+                return true;
+            } else return false;
+        }
+
+        private bool MissingComponent(string className, GameObject go, System.Type type) {
+            string self = (go != null) ? go.name : "errname";
+            string parent = (go.transform.parent != null) ? go.transform.parent.name : "none";
+            if (go.GetComponent(type) == null) {
+                Assert.That(false,"BUG: " + className + " is missing component " + type.ToString() + " on GameObject " + self + " with parent of " + parent);
+                return true;
+            } else return false;
+        }
+
+        private bool BoundsError(string className, GameObject go, int expectedMin, int expectedMax, int foundVal, string valueName) {
+            string self = (go != null) ? go.name : "errname";
+            string parent = (go.transform.parent != null) ? go.transform.parent.name : "none";
+            if (foundVal > expectedMax || foundVal < expectedMin) {
+                Assert.That(false,"BUG: " + className + " has invalid value for "+ valueName +" on GameObject " + self + " with parent of " + parent + ". Expected value in range: "
+                + expectedMin.ToString() + " to " + expectedMax.ToString() + "; Found value: " + foundVal.ToString());
+                return true;
+            } else return false;
+        }
 
         private bool BoundsError(string className, GameObject go, float expectedMin, float expectedMax, float foundVal, string valueName) {
             string self = (go != null) ? go.name : "errname";
             string parent = (go.transform.parent != null) ? go.transform.parent.name : "none";
             if (foundVal > expectedMax || foundVal < expectedMin) {
-                UnityEngine.Debug.Log("BUG: " + className + " has invalid value for " + valueName + " on GameObject " + self + " with parent of " + parent + ". Expected value in range: "
+                Assert.That(false,"BUG: " + className + " has invalid value for " + valueName + " on GameObject " + self + " with parent of " + parent + ". Expected value in range: "
                 + expectedMin.ToString() + " to " + expectedMax.ToString() + "; Found value: " + foundVal.ToString());
                 return true;
             } else return false;
