b760ea0e6812553bc3a2ded674fc0f849570f364
Josiah Jack <josiahjackcitadel@gmail.com>
2023-07-22 22:17:34 -0500

    Add first LevelManager unit test

---
+++ Assets/Tests/TestLevelManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
+
+namespace Tests {
+    public class TestLevelManager {
+        [UnityTest]
+        public IEnumerator TestAwakeWhiteroom() {
+            LevelManager lev = new LevelManager();
+            Const con = new Const();
+            Const.a = con;
+            GameObject camGO = new GameObject();
+            Const.a.player1CapsuleMainCameragGO = camGO;
+            Camera cam = camGO.AddCompenent<Camera>();
+            cam.useOcclusionCulling = true;
+            lev.currentLevel = -1; // Test area
+            lev.Awake();
+            
+            bool check = cam.useOcclusionCulling == false;
+            string msg = "Wrong camera culling settin for test area";
+            Assert.That(check,msg);
+        }
+    }
+}
