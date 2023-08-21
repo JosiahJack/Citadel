8ee969dd96fb4f2d918bcd9514e9fce2563b794b
Josiah Jack <josiahjackcitadel@gmail.com>
2023-08-20 21:39:47 -0500

    Restore build.sh to ye olden time of 30min ago

--- build.sh
+++ build.sh
@@ -6,14 +6,14 @@ UNITY_VERSION=2019.4.35f1
 GAME_CI_VERSION=1 # https://github.com/game-ci/docker/releases
 MY_USERNAME=josiahjack
 
-declare -a components=("android-2.0.0") #"linux-il2cpp" "mac-mono" "windows-mono")
+declare -a components=("linux-il2cpp" "mac-mono" "windows-mono")
 
 for component in "${components[@]}"
 do
-  GAME_CI_UNITY_EDITOR_IMAGE=unityci/editor:ubuntu-${UNITY_VERSION}-${component} #-${GAME_CI_VERSION}
-  IMAGE_TO_PUBLISH=${MY_USERNAME}/citadel-ci:ubuntu-${UNITY_VERSION}-${component} #-${GAME_CI_VERSION}
+  GAME_CI_UNITY_EDITOR_IMAGE=unityci/editor:ubuntu-${UNITY_VERSION}-${component}-${GAME_CI_VERSION}
+  IMAGE_TO_PUBLISH=${MY_USERNAME}/citadel-ci:ubuntu-${UNITY_VERSION}-${component}-${GAME_CI_VERSION}
   docker build --build-arg GAME_CI_UNITY_EDITOR_IMAGE=${GAME_CI_UNITY_EDITOR_IMAGE} . -t ${IMAGE_TO_PUBLISH}
 # uncomment the following to publish the built images to docker 
 # Do this whenever Unity version or host docker hub account changes
-  docker push ${IMAGE_TO_PUBLISH}
+# docker push ${IMAGE_TO_PUBLISH}
 done
