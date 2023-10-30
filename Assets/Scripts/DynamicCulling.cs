ea19e470138a9e0512a217638d7037311bd7752d
JosiahJack <josiahjackcitadel@gmail.com>
2023-10-29 21:18:03 -0500

    Dynamic culling progress

--- Assets/Scripts/DynamicCulling.cs
+++ Assets/Scripts/DynamicCulling.cs
@@ -7,17 +7,21 @@ public class DynamicCulling : MonoBehaviour {
     const int ARRSIZE = WORLDX * WORLDX;
     const float CELLX = 2.56f;
     const float CELLXHALF = 1.28f;
-    public bool[] worldCellOpen = new bool [ARRSIZE];
-    public Vector3[] worldCells = new Vector3 [ARRSIZE];
-    public List<GameObject>[] cells = new List<GameObject>[ARRSIZE];
-    public bool[] isDirty = new bool[ARRSIZE];
-    public int lastPlayerCell = 0;
-    public List<MeshRenderer> meshes;
-    public int playerCell = 0;
+    public bool[,] worldCellOpen = new bool [WORLDX,WORLDX];
+    public bool[,] worldCellVisible = new bool [WORLDX,WORLDX];
+    public Vector3[,] worldCellPositions = new Vector3 [WORLDX,WORLDX];
+    public List<GameObject>[,] cellLists = new List<GameObject>[WORLDX,WORLDX];
+    public List<MeshRenderer>[,] cellListsMR = new List<MeshRenderer>[WORLDX,WORLDX];
+    public bool[,] worldCellDirty = new bool[WORLDX,WORLDX];
+    public int lastPlayerCellX = 0;
+    public int lastPlayerCellY = 0;
+    public int playerCellX = 0;
+    public int playerCellY = 0;
     public float deltaX = 0.0f;
     public float deltaY = 0.0f;
     public Vector3 worldMax;
     public Vector3 worldMin;
+    public List<GameObject> orthogonalChunks;
 
     public static DynamicCulling a;
 
@@ -25,10 +29,7 @@ public class DynamicCulling : MonoBehaviour {
     // Plus CullResultsCreateShared: 0.20ms to 0.34ms
 
     // Plain radius based culling (this first iteration):  97ms !!
-
-    // These two were measured using Deep Profile on:
-    // RenderDeferred.Lighting Built-in: 2.8ms to 4.7ms, ~3ms average
-    // RednerDeferred.Lighting Maxewell: 
+    // Fixed: ___ms
 
     void Awake() {
         a = this;
@@ -42,168 +43,189 @@ public class DynamicCulling : MonoBehaviour {
         return true; 
     }
 
-    public void Cull_Init() {
-        List<GameObject> orthogonalChunks = new List<GameObject>();
-        orthogonalChunks.Clear();
-
-        for (int i=0;i<ARRSIZE;i++) {
-            cells[i] = new List<GameObject>();
-            cells[i].Clear();
-        }
-
-        // Determine open world cells at game start.
-		GameObject dCont = LevelManager.a.GetCurrentGeometryContainer();
-        if (dCont == null) return;
-
-        GameObject childGO = null;
-        Transform container = dCont.transform;
-        int chunkCount = container.childCount;
-        Vector3 mod = new Vector3(0f,0f,0f);
-        for (int i=0; i < chunkCount; i++) {
-			childGO = container.GetChild(i).gameObject;
-            if (EulerAnglesWithin90(childGO.transform.localEulerAngles)) {
-                mod = childGO.transform.localPosition;
-                mod.x = mod.x % CELLX;
-                mod.y = mod.y % CELLX;
-                mod.z = mod.z % CELLX;
-
-                // Only accept found chunks that are grid aligned
-                if (mod.x < 0.04f && mod.y < 0.04f && mod.z < 0.04f) {
-                    orthogonalChunks.Add(childGO);   
-                }
+    void ClearCellList() {
+        for (int x=0;x<64;x++) {
+            for (int y=0;y<64;y++) {
+                cellLists[x,y] = new List<GameObject>();
+                cellLists[x,y].Clear();
             }
-
-            Component[] compArray = childGO.GetComponentsInChildren(
-								    typeof(MeshRenderer),true);
-
-			foreach (MeshRenderer mr in compArray) meshes.Add(mr);
         }
+    }
 
-        Debug.Log("Found " + orthogonalChunks.Count.ToString() + " chunks");
-
-        // Find a reference point from which to build the grid.  Any orthogonal
-        // chunk will do.
-        Vector3 pos = orthogonalChunks[0].transform.localPosition;
-        Vector2 reference = new Vector2(pos.x,pos.z);
-
+    void FindWorldExtents(List<GameObject> chunks) {
         worldMax = new Vector3(0f,0f,0f);
         worldMin = new Vector3(0f,0f,0f);
-        // Find the grid extents by pretending reference is 0,0,0
-        for (int i=0; i < orthogonalChunks.Count; i++) {
-			childGO = orthogonalChunks[i].gameObject;
-            pos = childGO.transform.localPosition;
-            if (pos.x > (worldMax.x - reference.x)) worldMax.x = pos.x; 
-            if (pos.z > (worldMax.z - reference.y)) worldMax.z = pos.z;
-            if (pos.x < (worldMin.x - reference.x)) worldMin.x = pos.x; 
-            if (pos.z < (worldMin.z - reference.y)) worldMin.z = pos.z;            
+        Transform tr;
+        Vector3 pos;
+        for (int i=0; i < chunks.Count; i++) {
+			tr = chunks[i].transform;
+            pos = tr.position;
+            if (pos.x > worldMax.x) worldMax.x = pos.x;
+            if (pos.z > worldMax.z) worldMax.z = pos.z;
+            if (pos.x < worldMin.x) worldMin.x = pos.x;
+            if (pos.z < worldMin.z) worldMin.z = pos.z;
         }
+    }
 
+    void FindOpenCellsAndPositions(List<GameObject> chunks) {
         bool breakx = false;
         bool breaky = false;
         Vector2 pos2d = new Vector2(0f,0f);
         Vector2 pos2dcurrent = new Vector2(0f,0f);
-        for (int x=0; x < WORLDX; x++) {
+        GameObject childGO = null;
+        Vector3 pos;
+        for (int x=0; x<64; x++) {
             breakx = false;
             pos2dcurrent.x = worldMin.x + (CELLX * (float)x);
-            for (int z=0; z < WORLDX; z++) {
+            for (int y=0; y<64; y++) {
                 breaky = false;
-                pos2dcurrent.y = worldMin.z + (CELLX * (float)z);
+                pos2dcurrent.y = worldMin.z + (CELLX * (float)y);
                 for (int i=0; i < orthogonalChunks.Count; i++) {
                     childGO = orthogonalChunks[i].gameObject;
-                    pos = childGO.transform.localPosition;
+                    pos = childGO.transform.position;
                     pos2d.x = pos.x;
                     pos2d.y = pos.z;
                     if (Vector2.Distance(pos2d,pos2dcurrent) < 0.64f) {
-                        worldCellOpen[(z * 64) + x] = true;
-                        worldCells[(z * 64) + x] = pos;
-                        Debug.Log("Found open cell");
+                        worldCellOpen[x,y] = true;
+                        worldCellPositions[x,y] = pos;
                         breakx = breaky = true; break;
                     }
                 }
-                if (breaky) break;
+                if (breaky) continue;
             }
-            if (breakx) break;
+            if (breakx) continue;
         }
+    }
 
-        // Now go through every chunk and assign to a particular cell list.
+    void FindOrthogonalChunks(GameObject chunkContainer) {
+        orthogonalChunks = new List<GameObject>();
+        orthogonalChunks.Clear();
+        GameObject childGO = null;
+        Transform container = chunkContainer.transform;
+        int chunkCount = container.childCount;
+        for (int i=0; i < chunkCount; i++) {
+			childGO = container.GetChild(i).gameObject;
+            if (EulerAnglesWithin90(childGO.transform.localEulerAngles)) {
+                orthogonalChunks.Add(childGO);
+            }
+        }
+    }
+
+    void PutChunksInCells() {
+        Transform container = LevelManager.a.GetCurrentGeometryContainer().transform;
+        int chunkCount = container.childCount;
         bool[] alreadyInAtLeastOneList = new bool[chunkCount];
-        for (int i=0;i<ARRSIZE;i++) {
-            isDirty[i] = true;
-            for (int c=0;c<chunkCount;c++) {
-                if (alreadyInAtLeastOneList[c]) continue;
+        for (int c=0;c<chunkCount;c++) alreadyInAtLeastOneList[c] = false;
+        GameObject childGO = null;
+        Vector3 pos;
+        Vector2 pos2d = new Vector2(0f,0f);
+        Vector2 pos2dcurrent = new Vector2(0f,0f);
+        for (int x=0;x<64;x++) {
+            for (int y=0;y<64;y++) {
+                for (int c=0;c<chunkCount;c++) {
+                    if (alreadyInAtLeastOneList[c]) continue;
 
-                childGO = container.GetChild(c).gameObject;
-                pos = childGO.transform.localPosition;
-                pos2d.x = pos.x;
-                pos2d.y = pos.z;
-                pos2dcurrent.x = worldCells[i].x;
-                pos2dcurrent.y = worldCells[i].z;
-                if (Vector2.Distance(pos2d,pos2dcurrent) >= 1.28f) continue;
+                    childGO = container.GetChild(c).gameObject;
+                    pos = childGO.transform.position;
+                    pos2d.x = pos.x;
+                    pos2d.y = pos.z;
+                    pos2dcurrent.x = worldCellPositions[x,y].x;
+                    pos2dcurrent.y = worldCellPositions[x,y].z;
+                    if (Vector2.Distance(pos2d,pos2dcurrent) >= 1.28f) continue;
 
-                cells[i].Add(childGO);
-                alreadyInAtLeastOneList[c] = true;
-                Component[] compArray = childGO.GetComponentsInChildren(
-                                        typeof(MeshRenderer),true);
+                    cellLists[x,y].Add(childGO);
+                    alreadyInAtLeastOneList[c] = true;
+                    Component[] compArray = childGO.GetComponentsInChildren(
+                                              typeof(MeshRenderer),true);
 
-                foreach (MeshRenderer mr in compArray) {
-                    meshes.Add(mr);
-                    //mr.enabled = false;
+                    foreach (MeshRenderer mr in compArray) cellListsMR[x,y].Add(mr);
                 }
             }
         }
+    }
 
-        playerCell = 0;
-        // Find player cell
-        pos = PlayerMovement.a.transform.position;
-        for (int c=0;c<4096;c++) {
-            if (Vector3.Distance(pos,worldCells[c]) < 1.28f) {
-                playerCell = c;
+    void FindPlayerCell() {
+        playerCellX = 0;
+        playerCellY = 0;
+        Vector3 pos = PlayerMovement.a.transform.position;
+        for (int x=0;x<64;x++) {
+            for (int y=0;y<64;y++) {
+                if (Vector3.Distance(pos,worldCellPositions[x,y]) < 1.28f) {
+                    playerCellX = x;
+                    playerCellY = y;
+                    lastPlayerCellX = playerCellX;
+                    lastPlayerCellY = playerCellY;
+                    return;
+                }
             }
         }
-        lastPlayerCell = playerCell;
+    }
+
+    public void Cull_Init() {
+        ClearCellList();
+        FindOrthogonalChunks(LevelManager.a.GetCurrentGeometryContainer());
+        FindWorldExtents(orthogonalChunks);
+        FindOpenCellsAndPositions(orthogonalChunks);
+        PutChunksInCells();
+        FindPlayerCell();
+    }
+
+    bool UpdatedPlayerCell() {
+        Vector3 pos = PlayerMovement.a.transform.position;
+        deltaX = pos.x - worldCellPositions[playerCellX,playerCellY].x;
+        deltaY = pos.z - worldCellPositions[playerCellX,playerCellY].z;
+
+        if (deltaX > CELLXHALF) playerCellX++;
+        else if (deltaX < -CELLXHALF) playerCellX--;
+        if (playerCellX < 0) playerCellX = 0;
+        if (playerCellX > 63) playerCellX = 63;
+
+        if (deltaY > CELLXHALF) playerCellY++;
+        else if (deltaY < -CELLXHALF) playerCellY--;
+        if (playerCellY < 0) playerCellY = 0;
+        if (playerCellY > 63) playerCellY = 63;
+
+        if (   playerCellX == lastPlayerCellX
+            && playerCellY == lastPlayerCellY) {
+
+            return false; // No cell status updates.
+        }
+
+        lastPlayerCellX = playerCellX;
+        lastPlayerCellY = playerCellY;
+        return true;
+    }
+
+    void DetermineVisibleCells() {
+        for (int i=0;i<64;i++) {
+
+        }
+    }
+
+    void ToggleVisibility() {
+        for (int x=0;x<64;x++) {
+            for (int y=0;y<64;y++) {
+                if (!worldCellVisible[x,y] && !worldCellDirty[x,y]) return;
+
+                worldCellDirty[x,y] = false;
+                List<MeshRenderer> cellContents = cellListsMR[x,y];
+                for (int i=0;i<cellContents.Count;i++) {
+                    if (worldCellVisible[x,y]) {
+                        cellContents[i].enabled = true;
+                    } else {
+                        cellContents[i].enabled = false;
+                    }
+                }
+            }
+        }
     }
 
     public void Cull() {
-        Vector3 pos = PlayerMovement.a.transform.position;
-        deltaX = pos.x - worldCells[lastPlayerCell].x;
-        deltaY = pos.z - worldCells[lastPlayerCell].z;
-        if      (pos.x > worldMax.x || pos.x < worldMin.x) playerCell = 0;
-        else if (pos.y > worldMax.y || pos.y < worldMin.y) playerCell = 0;
-        else if (deltaX > CELLXHALF) { // Move current right
+        if (!UpdatedPlayerCell()) return;
 
-            playerCell = lastPlayerCell + 1;
+        DetermineVisibleCells(); // Reevaluate visible cells from new pos.
+        ToggleVisibility(); // Update all cells marked as dirty.
 
-            if      (deltaY > CELLXHALF) playerCell -= WORLDX; // Move up
-            else if (deltaY < -CELLXHALF) playerCell += WORLDX; // Move down
-        } else if (deltaX < -CELLXHALF) { // Move current left
-
-            playerCell = lastPlayerCell - 1;
-
-            if     (deltaY > CELLXHALF) playerCell -= WORLDX; // Move up
-            else if (deltaY < -CELLXHALF) playerCell += WORLDX; // Move down
-        }
-        
-        if (playerCell == lastPlayerCell) return; // No cell status updates.
-
-        //DetermineVisibleCells(); // Reevaluate visible cells from new pos.
-        //ToggleVisibility(); // Update all cells marked as dirty.
-        //return;
-
-
-
-
-
-        //if (lastPlayerCell != playerCell) {
-        //    for (int i=0;i<4096;i++) isDirty[i] = true; // TODO use vis!
-        //}
-
-        // Determine visibility of world cells by casting up to 252 rays along
-        // fixed integer angles based on going to center of each cell along
-        // world bounds, using open worldCellOpen status found previously.
-        // Then hide/unhide MeshRenderers accordingly.
-
-        // Iterate over all level chunks and adjust their visibility based on
-        // afore raycasting results.
         //int count = meshes.Count;
         //Vector2 pos2d = new Vector2(0f,0f);
         //bool visible = false;
