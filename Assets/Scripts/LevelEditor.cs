a33ed986a421b7ffa03beacab3a67eaf4ebfffa9
JosiahJack <josiahjackcitadel@gmail.com>
2023-12-20 19:57:55 -0600

    Placeholder input for the in-game level editor

--- Assets/Scripts/LevelEditor.cs
+++ Assets/Scripts/LevelEditor.cs
@@ -29,5 +29,27 @@ public class LevelEditor : MonoBehaviour {
         if (!Const.a.editMode) return;
 
         if (!inEditMode) EditorEntry();
+
+        if (MouseLookScript.a.inventoryMode) ToolMode();
+    }
+
+    void ToolMode() {
+        if (Input.GetKey(KeyCode.A)) { // Add Cell Tool
+
+        } else if (Input.GetKey(KeyCode.D)) { // Delete Cell Tool
+            Debug.Log("Delete Cell Tool");
+        } else if (Input.GetKey(KeyCode.E)) { // Enemy Insert Tool
+            Debug.Log("Enemy Insert Tool");
+        } else if (Input.GetKey(KeyCode.T)) { // Item Insert Tool
+            Debug.Log("Item Insert Tool");
+        } else if (Input.GetKey(KeyCode.W)) { // Light Insert Tool
+            Debug.Log("Light Insert Tool");
+        } else if (Input.GetKey(KeyCode.Q)) { // Quest and Misc Insert Tool
+            Debug.Log("Quest and Misc Insert Tool");
+        } else if (Input.GetKey(KeyCode.Z)) { // Cell Floor Height Tool
+            Debug.Log("Cell Floor Height Tool");
+        } else if (Input.GetKey(KeyCode.C)) { // Cell Ceiling Height Tool
+            Debug.Log("Cell Ceiling Height Tool");
+        }
     }
 }
