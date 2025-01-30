75d8a1e1b1919dcce9a86c92a84f30bcfef92e00
JosiahJack <josiahjackcitadel@gmail.com>
2025-01-29 22:26:52 -0600

    Update CI build scripts for new Unity Version

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 ARG GAME_CI_UNITY_EDITOR_IMAGE
 
 FROM $GAME_CI_UNITY_EDITOR_IMAGE
-#FROM unityci/editor:ubuntu-2019.4.35f1-linux-il2cpp-1
+#FROM unityci/editor:ubuntu-2020.3.48f1-linux-il2cpp-3
 
 RUN apt-get update && \
     apt-get install -y blender
--- build_unittest_docker.sh
+++ build_unittest_docker.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-UNITY_VERSION=2019.4.35f1
-GAME_CI_VERSION=1 # https://github.com/game-ci/docker/releases
+UNITY_VERSION=2020.3.48f1
+GAME_CI_VERSION=3 # https://github.com/game-ci/docker/releases
 MY_USERNAME=josiahjack
 
 # Just Linux image for unit test runner
@@ -14,7 +14,9 @@ do
   GAME_CI_UNITY_EDITOR_IMAGE=unityci/editor:ubuntu-${UNITY_VERSION}-${component}-${GAME_CI_VERSION}
   IMAGE_TO_PUBLISH=${MY_USERNAME}/citadel-ci:ubuntu-${UNITY_VERSION}-${component}-${GAME_CI_VERSION}
   docker build --build-arg GAME_CI_UNITY_EDITOR_IMAGE=${GAME_CI_UNITY_EDITOR_IMAGE} . -t ${IMAGE_TO_PUBLISH}
-# uncomment the following to publish the built images to docker 
-# Do this whenever Unity version or host docker hub account changes
- # docker push ${IMAGE_TO_PUBLISH}
+
+  # Do this whenever Unity version or host docker hub account changes
+  if [ "$1" == "push_docker" ]; then
+    docker push ${IMAGE_TO_PUBLISH}
+  fi
 done
